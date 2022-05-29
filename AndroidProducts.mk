f9370f3fe574043ebabe1f58033947e18adbfe6b
ItoRenz <itorenz@nusantararom.org>
2022-05-29 07:52:17 +0000

    spes: Voltage Bringup
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_spes.mk
+    $(LOCAL_DIR)/voltage_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_spes-user \
-    nad_spes-userdebug \
-    nad_spes-eng
+    voltage_spes-user \
+    voltage_spes-userdebug \
+    voltage_spes-eng
